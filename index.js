5e27697d827306c38aa07563d788a86809510e89
krishnaveni <krishnaveni042@gmail.com>
2024-12-30 20:35:32 +0530

    operators Q/A task

---
+++ index.js
@@ -0,0 +1,53 @@
+// 1. What is the result of 10 + '5'? //op ---> 105
+
+// 2. What is the value of x after x = 10; x += 5;? //op--->15
+
+// 3. What is the result of 10 - null? //op--->10
+
+// 4. What is the value of x after x = 10; x *= 2;? //op--->20
+
+// 5. What is the result of 10 / '2'? //op--->5
+
+// 6. What is the result of 10 % 3? //op--->1
+
+// 7. What is the value of x after x = 10; x = x ** 2;? //op--->100
+
+// 8. What is the result of 10 > 5 && 5 < 10? //op--->True
+
+// 9. What is the result of 10 === '10'? // op ---> False
+
+// 10. What is the result of null ?? 'default'? //o/p---> null
+
+// 11. What is the result of 10 || 'default'? //op---> 10
+
+// 12. What is the result of !true? //op--->false
+
+// 13. What is the result of 10 & 5? //op--->0
+
+// 14. What is the result of 10 | 5? //op---> 15
+
+// 15. What is the result of 10 ^ 5? //op--->15
+
+// 2. What is the result of x = 10; y = x++;? 
+// What are the values of x and y?    //op--->x=11 y=11
+
+// 3. What is the result of x = 10; console.log(++x);? // op---> 11
+
+// 4. What is the result of x = 10; console.log(x++);? //op--->10
+
+// 5. What is the result of x = 10; y = --x;?
+// What are the values of x and y?   //op---> x=9;y=9
+
+// 6. What is the result of x = 10; y = x--;? What are the values of x and y?  //op---> x=9;y=10
+
+// 1. What is the result of x = 10; y = x > 5 ? 'yes' : 'no';?  //op--->yes
+// 2. What is the value of y? // yes
+
+// 3. What is the result of x = 10; y = x > 5 ? 'yes' : x > 3 ? 'maybe' : 'no';?
+// 4. What is the value of y? //op--->yes
+
+// 1. What is the result of const obj = { a: { b: 'c' } }; console.log(obj?.a?.b);?  // c
+
+// 2. What is the result of const obj = { a: { b: 'c' } }; console.log(obj?.a?.c);? // undefined
+
+// 3. What is the result of const obj = { a: { b: 'c' } }; console.log(obj?.b?.c);? // undefined
